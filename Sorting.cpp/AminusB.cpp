f20f28a090153f4902a62309a76d9137230c5b8c
utkarsh1906-kesari <utkarshvns1906@gmail.com>
2026-09-14 09:46:37 +0530

    Sorting

--- Sorting.cpp/AminusB.cpp
+++ Sorting.cpp/AminusB.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> MergeArray(vector<int>A , vector<int>B){
+vector<int> AminusB(vector<int>A , vector<int>B){
     vector<int> C;
     int i = 0, j = 0;
     while(i < A.size() && j < B.size()){
@@ -48,7 +48,7 @@ int main(){
     B.push_back(6); 
     B.push_back(8);
     
-    C = MergeArray(A, B);
+    C = AminusB(A, B);
     for(int i = 0; i < C.size(); i++){
         cout << C[i] << " ";
     }
