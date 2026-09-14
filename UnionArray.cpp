22384c5f44354001c9677e90022fbd7c73d0962a
utkarsh1906-kesari <utkarshvns1906@gmail.com>
2026-09-14 09:39:36 +0530

    Union of Array

--- UnionArray.cpp
+++ UnionArray.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> MergeArray(vector<int>A , vector<int>B){
+vector<int> UnionArray(vector<int>A , vector<int>B){
     vector<int> C;
     int i = 0, j = 0 , k=0;
     while(i < A.size() && j < B.size()){
@@ -52,7 +52,7 @@ int main(){
     B.push_back(6); 
     B.push_back(8);
     
-    C = MergeArray(A, B);
+    C = UnionArray(A, B);
     for(int i = 0; i < C.size(); i++){
         cout << C[i] << " ";
     }
